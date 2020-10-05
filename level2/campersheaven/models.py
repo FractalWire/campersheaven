91a0a65da7589970a2938ef2c962de4aba82cec9
olivier G-R <olivier@fractalwire.io>
2020-10-06 00:22:02 +0200

    models: add Search and fields to Camper

--- level2/campersheaven/models.py
+++ level2/campersheaven/models.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
-from typing import Union
+from typing import Union, TYPE_CHECKING
 from dataclasses import dataclass, InitVar
+from datetime import datetime
 
 from .geometries import Point
 
@@ -15,6 +16,15 @@ class Camper:
     def __post_init__(self, latitude: float, longitude: float) -> None:
         if latitude and longitude:
             self.point = Point(longitude, latitude)
+
+    def search_price(self, search: Search) -> float:
+        """Get the price for the `Camper` between `start_date` and `end_date`"""
+        if not search.start_date:
+            return self.price_per_day
+
+        tdelta = search.end_date - search.start_date
+        discount_rate = (1 - (self.weekly_discount if tdelta.days >= 7 else 0))
+        return (self.price_per_day * tdelta.days) * discount_rate
 
 
 """ModelType is a dataclass, can't specify that"""
