7c7997670f4be3a8b818a6a8d5b2a28e84930673
olivier G-R <olivier@fractalwire.io>
2020-10-06 18:03:01 +0200

    implement stores for engine and reflect other changes

--- level3/campersheaven/engine.py
+++ level3/campersheaven/engine.py
@@ -16,13 +16,16 @@ class Engine:
 
     def __init__(self, camper_store: DictionaryStore,
                  calendar_store: DictionaryStore) -> None:
-        self.camper_store: DictionaryStore = camper_store
-        self.calendar_store: DictionaryStore = calendar_store
+        self.stores: Dict[DictionaryStore] = {
+            camper_store.name: camper_store,
+            calendar_store.name: calendar_store
+        }
 
     def insert_data(self, data: ByteString) -> None:
         """Insert new data to the store used by the Engine"""
         d = json.loads(data)
-        DataStoreAccess.populate_store(self.camper_store, d)
+        for store_name, store_data in d.items():
+            DataStoreAccess.populate_store(self.stores[store_name], store_data)
 
     def search(self, search_query: ByteString) -> ByteString:
         """Search for matching campers based on a query"""
@@ -32,11 +35,11 @@ class Engine:
         for search_query in d["searches"]:
             search = Search(**search_query)
 
-            campers = DataStoreAccess.find_campers_between_dates(
-                self.camper_store, search.point,
+            campers = DataStoreAccess.find_available_campers(
+                self.stores["campers"], search.point,
                 search.start_date, search.end_date)
-            searchcampers_tuple = (search, campers)
 
+            searchcampers_tuple = (search, campers)
             view_data.append(searchcampers_tuple)
 
         return View.render(view_data)
